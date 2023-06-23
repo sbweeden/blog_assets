f53732c30d641d8d2e4be8b27eebc024d1ad0cfc
Shane Weeden <sweeden@au1.ibm.com>
2023-06-23 16:21:07 +1000

    cleanup

--- fido_infomap/mapping_rules/FIDOInfomapRegistrations.js
+++ fido_infomap/mapping_rules/FIDOInfomapRegistrations.js
@@ -1,46 +1,6 @@
-importClass(Packages.com.tivoli.am.fim.trustserver.sts.utilities.IDMappingExtUtils);
 importClass(Packages.com.tivoli.am.fim.fido.mediation.FIDO2RegistrationHelper);
-importMappingRule("FIDOInfomapConfig");
 importMappingRule("FIDOInfomapUtils");
 
-function debugLog(s) {
-    IDMappingExtUtils.traceString(s);
-}
-
-function getInfomapUsername() {
-	// get username from already authenticated user
-	let result = context.get(Scope.REQUEST,
-			"urn:ibm:security:asf:request:token:attribute", "username");
-	debugLog("username from existing token: " + result);
-
-	// if not there, try getting from session (e.g. UsernamePassword module)
-	if (result == null) {
-		result = context.get(Scope.SESSION,
-				"urn:ibm:security:asf:response:token:attributes", "username");
-		debugLog("username from session: " + result);
-	}
-	if (result != null) {
-		// make it a javascript string - this prevents stringify issues later
-		result = '' + result;
-	}
-	return result;
-}
-
-/**
- * Utility to get the user unique id from the the Infomap context
- */
-function getInfomapUserUniqueID() {
-	let result = null;
-	let username = getInfomapUsername();
-	if (username != null) {
-		result = ScimClient.computeIDForUsername(username);
-	}
-	if (result != null) {
-		// make it a javascript string - this prevents stringify issues later
-		result = '' + result;
-	}
-	return result;
-}
 
 // returns JSON array of the current user's registrations
 function getRegistrations(frh) {
@@ -75,24 +35,12 @@ function deleteRegistration(frh, id) {
     }    
 }
 
-function sendJSONResponse(jObj) {
-    page.setValue("/authsvc/authenticator/fido_infomap/jsonresponse.html");
-    macros.put("@AUTHSVC_JSON_RESPONSE@", JSON.stringify(jObj));
-    responseProcessed = true;
-}
-
-function sendErrorResponse(str) {
-    page.setValue("/authsvc/authenticator/fido_infomap/error.html");
-    macros.put("@ERROR_MSG@", str);
-    responseProcessed = true;
-}
 
 /*
  * Main body starts here
  */
 var frh = new FIDO2RegistrationHelper();
 var lfc = fido2ClientManager.getClient(RPID);
-var responseProcessed = false;
    
 // you have to be logged in to use this infomap
 if (getInfomapUsername() == null) {
